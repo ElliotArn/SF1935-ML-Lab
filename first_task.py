01c31ea05adc43cbbee5fa75273d9861d46b1832
Teodor Bendréus <teodorbe@kth.se>
2025-05-09 17:16:07 +0200

    add: maybe correct code for calculating and plotting the likelihood

--- first_task.py
+++ first_task.py
@@ -13,10 +13,10 @@ sigma = np.sqrt(variance)
 
 # Step 1: prior distribution for w0 & w1
 mu = [0, 0]
-alpha = 2
+alpha = 0.5
 cov_matrix = (1 / alpha) * np.identity(2)
 
-linspace_size = 100
+linspace_size = 400
 w0_linspace = np.linspace(-5, 5, linspace_size)
 w1_linspace = np.linspace(-5, 5 , linspace_size)
 
@@ -30,19 +30,28 @@ plt.contour(w0arr, w1arr, wpriorpdf)
 plt.show()
 
 # Step 2: plot likelihood across all w in parameter space for subset data
-trn_data_size = 3
+trn_data_size = 10
 
 x_trn = np.linspace(-1, 1, 200)
 x_trn_sample = np.random.choice(x_trn, trn_data_size, False)
 t_trn_sample = w0_true + w1_true*x_trn_sample + np.random.normal(mean, sigma)
 
-x_tst = np.concatenate(np.linspace(-1.5, -1.1, 5), np.linspace(1.1, 1.5, 5))
-t_tst = w0_true + w1_true*x_tst + np.random.normal(mean, sigma)
+#x_tst = np.concatenate(np.linspace(-1.5, -1.1, 5), np.linspace(1.1, 1.5, 5))
+#t_tst = w0_true + w1_true*x_tst + np.random.normal(mean, sigma)
 
-w_transpose = np.transpose([w0_linspace, w1_linspace])
+likelihood_grid = np.zeros((linspace_size, linspace_size))
 
+# basically, what is the probability of finding the true t value (t_trn_sample) in a normal distribution with mean
+# t_prediction over all the different normal distributions (all pairs of (w0, w1))?
+for w0_index in range(linspace_size):
+    for w1_index in range(linspace_size):
+        w0_value = w0_linspace[w0_index]
+        w1_value = w1_linspace[w1_index]
 
+        t_prediction = w0_value + w1_value * x_trn_sample
+        likelihood = np.prod(multivariate_normal.pdf(t_trn_sample, t_prediction, variance))
 
+        likelihood_grid[w0_index, w1_index] = likelihood
 
-
-
+plt.contour(w0arr, w1arr, likelihood_grid)
+plt.show()
